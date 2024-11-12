138dbdd1752363cdba376144b3267c23b1d75a4c
IvanPopovych <byd0663@gmail.com>
2024-11-12 13:27:07 +0100

    idk which deply

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ITrips_front/',
   server: {
     host: '0.0.0.0',
     port: Number(process.env.PORT) || 4173,
